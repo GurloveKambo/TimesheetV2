0fd94df77c4fd56570d8f2c682635338c4acbeef
GurloveKambo <integrityneeds@gmail.com>
2026-05-01 21:26:11 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ================= VERSION ================= /
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = 'fieldsheet-' + CACHE_VERSION;
 
 / ================= FILES ================= /
